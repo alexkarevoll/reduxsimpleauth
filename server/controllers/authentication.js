a064be0b071186ee2a8911d6ba33ed442b6e42d3
alexkarevoll <alex.karevoll@gmail.com>
2017-06-07 16:24:16 -0700

    salting password

--- server/controllers/authentication.js
+++ server/controllers/authentication.js
@@ -1,3 +1,36 @@
+const User = require('../models/user');
+
 exports.signup = function(req, res, next) {
-  res.send({ success: 'true'});
+  const email = req.body.email;
+  const password = req.body.password;
+
+  if(!email || !password) {
+    return res.status(422).send({ error: 'email and password are required'});
+  }
+
+  // see if a user with a given email exists
+  User.findOne({ email: email }, function(err, existingUser) {
+    if(err) { return next(err); }
+
+    // if a user with an email does exist, return an error
+    if(existingUser) {
+      return res.status(422).send({ error: 'Email is already in use' });
+    }
+
+    // if a user with email does not exist, create and save record
+    const user = new User({
+      email: email,
+      password: password
+    });
+
+    user.save(function(err) {
+      if(err) { return next(err); }
+
+    // respond to the request indicating the user was created
+    res.json({"success": "true"});
+    });
+
+  });
+  
+
 }
