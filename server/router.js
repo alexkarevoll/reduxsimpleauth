662d24d17993fdce331d69d1a5f784a4e564c4de
alexkarevoll <alex.karevoll@gmail.com>
2017-06-28 17:55:35 -0700

    local passport strategy and signin

--- server/router.js
+++ server/router.js
@@ -4,10 +4,12 @@ const passport = require('passport');
 
 // session: false so that it doesn't use cookies
 const requireAuth = passport.authenticate('jwt', { session: false });
+const requireSignin = passport.authenticate('local', { session: false });
 
 module.exports = function(app) {
   app.get('/', requireAuth, function(req, res) {
     res.send({ hi: 'there' });
   });
+  app.post('/signin', requireSignin, Authentication.signin)
   app.post('/signup', Authentication.signup);
 }
