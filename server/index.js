34c79aba79ff9a38b0f7d0deeb542ba3e17f24dd
alexkarevoll <alex.karevoll@gmail.com>
2017-07-06 19:49:18 -0700

    cors and protected route message

--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const app = express();
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 const router = require('./router');
 
@@ -13,6 +14,7 @@ mongoose.connect('mongodb://localhost:auth/auth');
 
 // App setup
 app.use(morgan('combined'));
+app.use(cors());
 app.use(bodyParser.json());
 router(app);
 
